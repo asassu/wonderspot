538bc8026e65a49b9ae048e21c13e7f9eb39a304
Andrea <aksassu@gmail.com>
2013-12-02 10:17:37 -0700

    Basic users and microposts

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :name, :email, :description, :interests, :quotes, :created_at, :updated_at
---
+++ db/migrate/20131202171017_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :description
+      t.string :interests
+      t.string :quotes
+
+      t.timestamps
+    end
+  end
+end
