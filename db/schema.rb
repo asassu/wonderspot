51af12bf1c17a9f531fe0661ccc6996b493e8a1f
Andrea <aksassu@gmail.com>
2013-12-02 23:13:47 -0700

    Basic User model (with password, email, and name validation)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202171418) do
+ActiveRecord::Schema.define(version: 20131203054730) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20131202171418) do
     t.string   "quotes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
