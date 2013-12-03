a699d11e54342a97204b4d19d32cfad939bda8d1
Andrea <aksassu@gmail.com>
2013-12-02 21:18:18 -0700

    Basic layout stuff

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,31 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'Midnight'" do
-      visit '/static_pages/home'
+
+    it "should have the h1 'Midnight'" do
+      visit root_path
       expect(page).to have_content('Midnight')
     end
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Home")
+
+    it "should have the home title" do
+      visit root_path
+      expect(page).to have_title("Midnight - Home")
     end
+
   end
+
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+
+    it "should have the h1 'About'" do
+      visit about_path
+      expect(page).to have_content('About')
     end
+
     it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("About")
+      visit about_path
+      expect(page).to have_title("Midnight - About")
     end
   end
 end
