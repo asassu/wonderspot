655a988aee0f9a57e503968337405572f8e6d04a
Andrea <aksassu@gmail.com>
2013-12-03 19:19:37 -0700

    Not rcognizing remember_token

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Midnight::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/register',  to: 'users#new',          via: 'get'
+  match '/signin',    to: 'sessions#new',       via: 'get'
+  match '/signout',   to: 'sessions#destroy',   via: 'delete'
   match '/help',      to: 'static_pages#help',  via: 'get'
   match '/about',     to: 'static_pages#about', via: 'get'
 
